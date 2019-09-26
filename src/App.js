5a5a5b80b16da831cee0065820013531a4a114c6
Ratna Lama <rlama@Karen-Sutton-MacBook-Pro.local>
2019-09-25 17:47:20 -0700

    added scroll components.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
-import "./App.css";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
+import Scroll from "./Scroll";
+import "./App.css";
+
 
 class App extends Component {
   // state
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ""
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({robots: users}))
   }
 
   onSearchChange = event => {
@@ -23,14 +30,22 @@ class App extends Component {
       return robots.name
         .toLowerCase()
         .includes(this.state.searchField.toLowerCase());
-    });
-    return (
-      <div className="tc">
-        <h1 className="f2"> RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    })
+
+    // if takes too long to fetch then response with loading message
+    if (this.state.robots.length === 0) {
+      return <h1>Loading ...</h1>
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f2"> RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+          <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
   }
 }
 
